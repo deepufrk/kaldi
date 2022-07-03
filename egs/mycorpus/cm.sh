2344ff5fc6ff5e0ec15173f3bba4582ee96e41c3
Siddharth Gupta <siddharthgupta234@gmail.com>
2022-07-03 19:21:58 +0530

    add cm.sh file

---
+++ egs/mycorpus/cm.sh
@@ -0,0 +1,7 @@
+# Press i to insert; esc to exit insert mode; 
+# ‘:wq’ to write and quit; ‘:q’ to quit normally; 
+# ‘:q!’ to quit forcibly (without saving)
+
+# Insert the following text in cmd.sh
+train_cmd="queue.pl"
+decode_cmd="queue.pl  --mem 2G"
